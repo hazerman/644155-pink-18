10e6a3db1e84e6a2e4f428cb3ba207567353c1ff
hazerman <49827421+hazerman@users.noreply.github.com>
2020-01-03 17:22:06 +0500

    Добавляет исключения для линтера

--- gulpfile.js
+++ gulpfile.js
@@ -106,7 +106,6 @@ gulp.task("server", function() {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
-  // gulp.watch("source/img/**/*.{png,jpg,svg}", gulp.series("images", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
 });
