183f36c494e96cd7a6f7e3e89ca716df31e67ecf
hazerman <49827421+hazerman@users.noreply.github.com>
2020-01-19 21:41:20 +0500

    Доделывает скрипты для слайдеров

--- source/js/price-slider.js
+++ source/js/price-slider.js
@@ -1,11 +1,15 @@
-var priceWrapper = document.querySelector(".price");
-var priceContainer = priceWrapper.querySelector(".price__container");
-var priceTableWrapper = priceContainer.querySelector(".price__table-wrapper");
-var priceRates = priceWrapper.querySelectorAll(".price__title");
+"use strict"
 
-if (priceWrapper) {
+if (document.querySelector(".price")) {
+  var priceWrapper = document.querySelector(".price");
+  var priceContainer = priceWrapper.querySelector(".price__container");
+  var priceTableWrapper = priceContainer.querySelector(".price__table-wrapper");
+  var priceTable = priceTableWrapper.querySelector(".price__table");
+  var priceRates = priceWrapper.querySelectorAll(".price__title");
 
   priceTableWrapper.classList.remove("price__table-wrapper--no-js");
+  priceRates[2].classList.add("price__title--current");
+  priceTable.classList.add("price__table--show-2-slide");
 
   var priceDotList = document.createElement("ul");
   priceDotList.classList.add("dot-list");
@@ -33,11 +37,33 @@ if (priceWrapper) {
     var CurSlide = 0;
     for (var i = 1; i < priceRates.length; i++) {
       if (priceRates[i].classList.contains("price__title--current")) {
-        CurSlide = i-1;
+        CurSlide = i - 1;
       }
     }
     return CurSlide;
   };
 
   priceDotBtn[findCurrentRate()].classList.add("dot-list__button--active");
+
+  var changeSlide = function (btn, slide, counter) {
+    btn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+
+      var currentSlide = findCurrentRate();
+      var startString = "price__table--show-"
+      var finishString = "-slide";
+
+      priceDotBtn[currentSlide].classList.remove("dot-list__button--active");
+      priceRates[currentSlide + 1].classList.remove("price__title--current");
+      priceTable.classList.add(startString + counter + finishString);
+      priceTable.classList.remove(startString + (currentSlide + 1) + finishString);
+
+      btn.classList.add("dot-list__button--active");
+      slide.classList.add("price__title--current");
+    })
+  }
+
+  for (var i = 0; i < priceRates.length - 1; i++) {
+    changeSlide(priceDotBtn[i], priceRates[i+1], i+1);
+  }
 }
--- source/js/reviews-slider.js
+++ source/js/reviews-slider.js
@@ -1,11 +1,12 @@
-var reviewsWrapper = document.querySelector(".reviews");
-var reviewsContainer = reviewsWrapper.querySelector(".reviews__container");
-var reviewsList = reviewsWrapper.querySelector(".reviews__list");
-var reviewsItems = reviewsList.querySelectorAll(".reviews__item");
-var reviewsBtnPrev = reviewsContainer.querySelector(".reviews__slide-button--prev");
-var reviewsBtnNext = reviewsContainer.querySelector(".reviews__slide-button--next");
+"use strict"
 
-if (reviewsWrapper) {
+if (document.querySelector(".reviews")) {
+  var reviewsWrapper = document.querySelector(".reviews");
+  var reviewsContainer = reviewsWrapper.querySelector(".reviews__container");
+  var reviewsList = reviewsWrapper.querySelector(".reviews__list");
+  var reviewsItems = reviewsList.querySelectorAll(".reviews__item");
+  var reviewsBtnPrev = reviewsContainer.querySelector(".reviews__slide-button--prev");
+  var reviewsBtnNext = reviewsContainer.querySelector(".reviews__slide-button--next");
 
   for (var i = 1; i < reviewsItems.length; i++) {
     reviewsItems[i].classList.add("reviews__item--hidden");
@@ -32,7 +33,7 @@ if (reviewsWrapper) {
 
     var reviewsAlly = document.createElement("span");
     reviewsAlly.classList.add("visually-hidden");
-    reviewsAlly.textContent = (i+1) + " слайд";
+    reviewsAlly.textContent = i + 1 + " слайд";
     reviewsDotBtn[i].appendChild(reviewsAlly);
   }
 
@@ -44,7 +45,60 @@ if (reviewsWrapper) {
       }
     }
     return CurSlide;
+  };
+
+  var changeSlide = function (btn, slide) {
+    btn.addEventListener("click", function (evt) {
+      evt.preventDefault();
+      var currentSlide = findCurrentSlide();
+
+      reviewsDotBtn[currentSlide].classList.remove("dot-list__button--active");
+      reviewsItems[currentSlide].classList.add("reviews__item--hidden");
+
+      btn.classList.add("dot-list__button--active");
+      slide.classList.remove("reviews__item--hidden");
+    })
+  }
+
+  var goNextSlide = function () {
+    var currentSlide = findCurrentSlide();
+    reviewsItems[currentSlide].classList.add("reviews__item--hidden");
+    reviewsDotBtn[currentSlide].classList.remove("dot-list__button--active");
+
+    if (currentSlide === reviewsItems.length - 1) {
+      reviewsItems[0].classList.remove("reviews__item--hidden");
+      reviewsDotBtn[0].classList.add("dot-list__button--active");
+    } else {
+      reviewsItems[currentSlide + 1].classList.remove("reviews__item--hidden");
+      reviewsDotBtn[currentSlide + 1].classList.add("dot-list__button--active");
+    }
+  }
+
+  var goPreviousSlide = function () {
+    var currentSlide = findCurrentSlide();
+    reviewsItems[currentSlide].classList.add("reviews__item--hidden");
+    reviewsDotBtn[currentSlide].classList.remove("dot-list__button--active");
+
+    if (currentSlide === 0) {
+      reviewsItems[reviewsItems.length - 1].classList.remove("reviews__item--hidden");
+      reviewsDotBtn[reviewsItems.length - 1].classList.add("dot-list__button--active");
+    } else {
+      reviewsItems[currentSlide - 1].classList.remove("reviews__item--hidden");
+      reviewsDotBtn[currentSlide - 1].classList.add("dot-list__button--active");
+    }
   }
 
   reviewsDotBtn[findCurrentSlide()].classList.add("dot-list__button--active");
+
+  for (var i = 0; i < reviewsItems.length; i++) {
+    changeSlide(reviewsDotBtn[i], reviewsItems[i]);
+  }
+
+  reviewsBtnNext.addEventListener("click", function (evt) {
+    goNextSlide();
+  })
+
+  reviewsBtnPrev.addEventListener("click", function (evt) {
+    goPreviousSlide();
+  })
 }
